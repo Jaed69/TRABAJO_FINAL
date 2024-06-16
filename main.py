ed03b4a9c62dd667beb662179470b69ea7960249
Fig's <pencarjh_99@hotmail.com>
2024-06-15 21:26:54 -0500

    Creacion de grafios usando Graphviz

--- main.py
+++ main.py
@@ -1,94 +1,78 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QPushButton, QVBoxLayout, QWidget, QLineEdit, QLabel
-from PyQt5.QtGui import QPainter, QBrush, QPen
+import random
+import networkx as nx
+import pygraphviz as pgv
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 
-class Graph:
-    def __init__(self):
-        self.nodes = []
-        self.edges = []
+def create_random_graph():
+    G = nx.gnp_random_graph(10, 0.3, directed=True)
+    for (u, v) in G.edges():
+        G.edges[u, v]['weight'] = random.randint(1, 10)
+    return G
 
-    def add_node(self, node):
-        self.nodes.append(node)
+def draw_graph(G, filename):
+    A = nx.nx_agraph.to_agraph(G)
+    A.layout(prog='dot')
+    A.draw(filename)
 
-    def add_edge(self, edge):
-        self.edges.append(edge)
+def apply_dijkstra(G, source):
+    return nx.single_source_dijkstra_path(G, source)
 
-class GraphView(QGraphicsView):
-    def __init__(self, graph, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.graph = graph
-        self.scene = QGraphicsScene()
-        self.setScene(self.scene)
-        self.update_view()
-
-    def update_view(self):
-        self.scene.clear()
-        for node in self.graph.nodes:
-            self.scene.addEllipse(node[0], node[1], 20, 20, QPen(Qt.black), QBrush(Qt.blue))
-        for edge in self.graph.edges:
-            self.scene.addLine(edge[0][0], edge[0][1], edge[1][0], edge[1][1], QPen(Qt.black))
-
-class MainWindow(QMainWindow):
+class DijkstraApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        self.layout = QVBoxLayout()
+        
+        self.graph_label = QLabel("Grafo Original", self)
+        self.layout.addWidget(self.graph_label)
+        
+        self.graph_view = QGraphicsView(self)
+        self.layout.addWidget(self.graph_view)
+        
+        self.result_label = QLabel("Grafo con Dijkstra aplicado", self)
+        self.layout.addWidget(self.result_label)
+        
+        self.result_view = QGraphicsView(self)
+        self.layout.addWidget(self.result_view)
+        
+        self.btn = QPushButton('Generar Grafo y Aplicar Dijkstra', self)
+        self.btn.clicked.connect(self.generate_graph)
+        self.layout.addWidget(self.btn)
+        
+        self.setLayout(self.layout)
+        self.setWindowTitle('Visualización de Dijkstra')
+        self.show()
+    
+    def generate_graph(self):
+        G = create_random_graph()
+        
+        # Guardar y mostrar el grafo original
+        draw_graph(G, 'graph_before.png')
+        pixmap = QPixmap('graph_before.png')
+        scene = QGraphicsScene()
+        scene.addItem(QGraphicsPixmapItem(pixmap))
+        self.graph_view.setScene(scene)
+        
+        # Aplicar Dijkstra
+        source = list(G.nodes())[0]
+        dijkstra_path = apply_dijkstra(G, source)
+        
+        # Dibujar y mostrar el grafo con Dijkstra aplicado
+        H = G.copy()
+        edges_in_path = [(dijkstra_path[node], node) for node in dijkstra_path if node != source]
+        nx.set_edge_attributes(H, {edge: {'color': 'red'} for edge in edges_in_path})
+        draw_graph(H, 'graph_after.png')
+        pixmap = QPixmap('graph_after.png')
+        scene = QGraphicsScene()
+        scene.addItem(QGraphicsPixmapItem(pixmap))
+        self.result_view.setScene(scene)
 
-        self.graph = Graph()
-        self.graph_view = GraphView(self.graph)
-
-        self.node_x_input = QLineEdit(self)
-        self.node_y_input = QLineEdit(self)
-        self.add_node_button = QPushButton("Add Node", self)
-        self.add_node_button.clicked.connect(self.add_node)
-
-        self.edge_start_input = QLineEdit(self)
-        self.edge_end_input = QLineEdit(self)
-        self.add_edge_button = QPushButton("Add Edge", self)
-        self.add_edge_button.clicked.connect(self.add_edge)
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.graph_view)
-        layout.addWidget(QLabel("Node X:"))
-        layout.addWidget(self.node_x_input)
-        layout.addWidget(QLabel("Node Y:"))
-        layout.addWidget(self.node_y_input)
-        layout.addWidget(self.add_node_button)
-        layout.addWidget(QLabel("Edge Start (node index):"))
-        layout.addWidget(self.edge_start_input)
-        layout.addWidget(QLabel("Edge End (node index):"))
-        layout.addWidget(self.edge_end_input)
-        layout.addWidget(self.add_edge_button)
-
-        container = QWidget()
-        container.setLayout(layout)
-
-        self.setCentralWidget(container)
-        self.setWindowTitle("Graph Visualizer")
-        self.setGeometry(100, 100, 800, 600)
-
-    def add_node(self):
-        try:
-            x = int(self.node_x_input.text())
-            y = int(self.node_y_input.text())
-            self.graph.add_node((x, y))
-            self.graph_view.update_view()
-        except ValueError:
-            pass
-
-    def add_edge(self):
-        try:
-            start_index = int(self.edge_start_input.text())
-            end_index = int(self.edge_end_input.text())
-            if start_index < len(self.graph.nodes) and end_index < len(self.graph.nodes):
-                start_node = self.graph.nodes[start_index]
-                end_node = self.graph.nodes[end_index]
-                self.graph.add_edge((start_node, end_node))
-                self.graph_view.update_view()
-        except ValueError:
-            pass
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+    ex = DijkstraApp()
     sys.exit(app.exec_())
